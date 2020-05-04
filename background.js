0548dd8468d7de2673b76b8761d96a3866ae5cb2
abobrov <andreybobrovdev@gmail.com>
2020-05-04 21:31:46 +0200

    tracking of the back / forward / closed tab

--- background.js
+++ background.js
@@ -1,71 +1,28 @@
-class DataSessionEntity {
-    constructor(date, elapsedMs, hostName, category) {
-        this.date = date;
-        this.elapsedMs = elapsedMs;
-        this.hostName = hostName;
-        this.category = category;
-    }
+var lastActiveTabData = {
+    tabId: 0,
+    windowId: 0,
+    hostName: ""
 }
-;
-class SiteDataEntity {
-    constructor(hostName) {
-        this.hostName = hostName;
-        this.sessions = [];
-        this.addSession = (pushData) => {
-            this.sessions.push(new DataSessionEntity(pushData.date, pushData.elapsedMs, pushData.hostName, pushData.category));
-        };
-        this.totalElapsedMsForDate = (date) => {
-            return this.sessions.filter((s) => {
-                s.date.getYear() == date.getYear()
-                    && s.date.getMonth() == date.getMonth()
-                    && s.date.getDate() == date.getDate();
-            }).reduce((s1, s2) => s1 + s2.elapsedMs, 0);
-        };
-        this.totalSiteTimeForDate = (date) => new Date(this.totalElapsedMsForDate(date));
-    }
-}
-class StorageDateEntity {
-    constructor(date) {
-        this.date = date;
-        this.dateKey = date.toDateString();
-        this.sites = [];
-        this.getSiteByHostName = (hostName) => this.sites.find(s => s.hostName == hostName);
-        this.totalTimeMs = () => this.sites.reduce((s1, s2) => s1 + s2.totalElapsedMsForDate(date), 0);
-        this.totalTime = () => new Date(totalTimeMs());
-        this.addOrUpdateSite = (pushData) => {
-            var siteEntry = this.getSiteByHostName(pushData.hostName);
-            // if siteEntry is null, we need to push a new one to the array
-            var isNew = siteEntry ? true : false;
-            siteEntry = siteEntry ?? new SiteDataEntity(pushData.hostName);
-            siteEntry.addSession(pushData);
-            if (isNew) {
-                this.sites.push(siteEntry);
-            }
-        };
-    }
-}
-class StorageDataObject {
-    constructor() {
-        this.dates = [];
-        this.addDate = (storageDateEntity) => this.dates.push(storageDateEntity);
-        this.getDate = (date) => this.dates.find(d => d.dateKey == date.toDateString());
-        this.addOrUpdate = (pushData) => {
-            var date = pushData.date;
-            var dateEntry = this.getDate(date);
-            var isNew = dateEntry ? true : false;
-            dateEntry = dateEntry ?? new StorageDateEntity(date);
-            dateEntry.addOrUpdateSite(pushData);
-            if (isNew)
-                this.dates.push(dateEntry);
-        };
-    }
-}
+
+var getStorageKeyForTab = (tabId, windowId, key) => {
+    return key + "_" + windowId + "_" + tabId;
+};
 
 var findCategoryByWebsiteInfo = (data) => {
     // Coming soon
     // <meta property="og:type" content="article">
     // <meta property="og:type" content="product"></meta>
     return "Other";
+}
+
+var collectFavIconUrlFromTheTab = (tab) => {
+    return tab.favIconUrl || false
+        ? new Promise((resolve, reject) => resolve(tab.favIconUrl))
+        : new Promise((resolve, reject) => {
+            chrome.tabs.sendMessage(tab.id, { type: "collectPageTags" }, null, (response) => {
+                resolve((response && response.favIconUrl) || "");
+            });
+        })
 }
 
 var sendToStorage = (key, obj) => {
@@ -88,33 +45,6 @@ var addNewTimeCollectionEntry = (data) => {
         category: findCategoryByWebsiteInfo(data)
     }
 
-    // // save to the mass storage
-    // chrome.storage.local.get("TimeSessionsBySite", (storageResultObj) => {
-    //     var TimeSessionsBySite = storageResultObj && storageResultObj["TimeSessionsBySite"] || {};
-    //     var timeSessionByHostEntry = TimeSessionsBySite[hostNameKey] || {};
-    //     var dataArr = (timeSessionByHostEntry.arr) || [];
-
-    //     dataArr.push(currentSessionData);
-
-    //     TimeSessionsBySite[hostNameKey] = timeSessionByHostEntry =
-    //     {
-    //         hostName: currentSessionData.hostName,
-    //         favIconUrl: data.favIconUrl,
-    //         grad: timeSessionByHostEntry.grad,
-    //         arr: dataArr
-    //     };
-
-    //     // If has gradient assigned
-    //     if (timeSessionByHostEntry.grad ?? false) {
-    //         sendToStorage("TimeSessionsBySite", TimeSessionsBySite);
-    //     } else {
-    //         getColorsFromFavUrl(data.favIconUrl).then((grad) => {
-    //             TimeSessionsBySite[hostNameKey].grad = grad;
-    //             sendToStorage("TimeSessionsBySite", TimeSessionsBySite);
-    //         });
-    //     }
-    // });
-
     // save by the day key
     var todayString = new Date().toDateString();
     chrome.storage.local.get([todayString], (storageResultObj) => {
@@ -130,112 +60,118 @@ var addNewTimeCollectionEntry = (data) => {
         var sessionsByHostname = (timeSessionByHostEntry.sessions) || [];
         sessionsByHostname.push(currentSessionData);
 
-        timeSessionsForToday.siteSessionData[hostNameKey] =
-        {
-            hostName: currentSessionData.hostName,
-            favIconUrl: data.favIconUrl,
-            grad: timeSessionByHostEntry.grad,
-            sessions: sessionsByHostname
-        };
+        timeSessionByHostEntry.hostName = currentSessionData.hostName || timeSessionByHostEntry.hostName;
+        timeSessionByHostEntry.favIconUrl = data.favIconUrl || timeSessionByHostEntry.favIconUrl;
+        timeSessionByHostEntry.grad = timeSessionByHostEntry.grad;
+        timeSessionByHostEntry.sessions = sessionsByHostname;
+
+        timeSessionsForToday.siteSessionData[hostNameKey] = timeSessionByHostEntry;
         // If has gradient assigned
         if (timeSessionByHostEntry.grad ?? false) {
             sendToStorage([todayString], timeSessionsForToday);
         } else {
-            getColorsFromFavUrl(data.favIconUrl).then((grad) => {
-                timeSessionsForToday.siteSessionData[hostNameKey].grad = grad;
-                sendToStorage([todayString], timeSessionsForToday);
-            });
+            getColorsFromFavUrl(data.favIconUrl)
+                .then((grad) => {
+                    timeSessionsForToday.siteSessionData[hostNameKey].grad = grad;
+                    sendToStorage([todayString], timeSessionsForToday);
+                })
+                // if impossible to get favicon - save anyway
+                .error(() => sendToStorage([todayString], timeSessionsForToday));
         }
     });
 }
 
-var getStorageKeyForTab = (tab, key) => {
-    return key + "_" + tab.windowId + "_" + tab.id;
-}
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.contextMenus.create({
-        "id": "sampleContextMenu",
-        "title": "Sample Context Menu",
-        "contexts": ["selection"]
+var saveSessionData = (tabId, windowId, hostname, favIconUrl) => {
+    var start_key = getStorageKeyForTab(tabId, windowId, "start");
+    var startDate = chrome.storage.sync[start_key] ?? new Date();
+    var elapsedMs = new Date() - startDate;
+
+    addNewTimeCollectionEntry({
+        startDate: startDate,
+        elapsedMs: elapsedMs,
+        hostName: hostname,
+        favIconUrl: favIconUrl
     });
-});
+    // empty local storage for this tab
+    chrome.storage.sync.remove([start_key]);
+}
 
-chrome.runtime.onStartup.addListener(function () {
+var collectTabData = (tab) => {
+    var pendingUrl = tab.pendingUrl;
+    var lastUrl = tab.url;
+    var pendingUrlHostName = tab.pendingUrl && new URL(pendingUrl).host || null;
+    var lastUrlHostName = new URL(lastUrl).host;
 
-});
+    // if navigated to a different host name
+    if (lastUrlHostName != pendingUrlHostName
+        // or there is no pending host name provided (tab switch)
+        || !pendingUrlHostName) {
+        collectFavIconUrlFromTheTab(tab)
+            .then((favIconUrl) => {
+                saveSessionData(tab.tabId, tab.windowId, lastUrlHostName, favIconUrl);
+            })
+    }
+}
 
 // when something inside a tab (active or not) happens
 // i.e. telegram notification
 // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-//     console.log(tab.active);
-//     console.log(tab.url);
-//     console.log(tab.title);
 // })
 
-chrome.webNavigation.onCompleted.addListener((details) => {
+chrome.webNavigation.onCommitted.addListener((details) => {
     chrome.tabs.get(details.tabId, (tab) => {
-        var start_key = getStorageKeyForTab(tab, "start");
+        if (tab.frameId != 0) return;
+        var start_key = getStorageKeyForTab(tab.tabId, tab.windowId, "start");
         chrome.storage.sync[start_key] = new Date();
     });
-
 });
-
-var collectFavIconUrlFromTheTab = (tab) => {
-    return tab.favIconUrl || false
-        ? new Promise((resolve, reject) => resolve(tab.favIconUrl))
-        : new Promise((resolve, reject) => {
-            chrome.tabs.sendMessage(tab.id, { type: "collectPageTags" }, null, (response) => {
-                resolve(response.favIconUrl);
-            });
-        })
-}
 
 // This event is fired when we havigate to a different page within one tab 
 // OR go back 
 // OR hit ENTER in the address bar
 // So we need to save the session data ONLY if the target navigation URL is different 
 chrome.webNavigation.onBeforeNavigate.addListener((evt) => {
-    // https://developer.chrome.com/extensions/tabs#method-query
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-        var activeTab = tabs[0];
+    // Means frame is not parent
+    // https://stackoverflow.com/questions/37779142/chrome-webnavigation-oncompleted-event-firing-multiple-times
+    if (evt.frameId != 0) return;
 
+    chrome.tabs.query({ active: true }, (tabs) => {
+        var activeTab = tabs[0];
         if (!activeTab)
             return console.error("active tab was null");
-
-        var favIconUrl = activeTab.favIconUrl;
-        var pendingUrl = activeTab.pendingUrl;
-        var lastUrl = activeTab.url;
-        var pendingUrlHostName = new URL(pendingUrl).host;
-        var lastUrlHostName = new URL(lastUrl).host;
-
-        // if navigated to a different host name
-        if (lastUrlHostName != pendingUrlHostName) {
-            collectFavIconUrlFromTheTab(activeTab)
-                .then((favIconUrl) => {
-                    var start_key = getStorageKeyForTab(activeTab, "start");
-                    var startDate = chrome.storage.sync[start_key] ?? new Date();
-                    var elapsedMs = new Date() - startDate;
-
-                    addNewTimeCollectionEntry({
-                        startDate: startDate,
-                        elapsedMs: elapsedMs,
-                        hostName: lastUrlHostName,
-                        favIconUrl: favIconUrl
-                    });
-                    // empty local storage for this tab
-                    chrome.storage.sync.remove([start_key]);
-                })
-        }
+        // collect active tab data before navigated to different browser url
+        collectTabData(activeTab);
     });
 
 });
 
-// Start the clock when tab has became active
+// Start the clock when tab has become active
 chrome.tabs.onActivated.addListener((activeInfo) => {
+    // Collect data from the previous tab
+    chrome.tabs.get(lastActiveTabData.tabId, (lastActiveTab) => {
+        if (!lastActiveTab) return;
+        // collect data
+        collectTabData(lastActiveTab);
+    });
+
     chrome.tabs.get(activeInfo.tabId, (tab) => {
-        var start_key = getStorageKeyForTab(tab, "start");
+        // preserve this tab data for future
+        lastActiveTabData.tabId = tab.id;
+        lastActiveTabData.windowId = tab.windowId;
+        lastActiveTabData.hostName = new URL(tab.url).host;
+
+        var start_key = getStorageKeyForTab(tab.tabId, tab.windowId, "start");
         chrome.storage.sync[start_key] = new Date();
     });
 });
 
-chrome.tabs.onRemoved.addListener((tabId, removeInfo) => { });
+// chrome.tabs.onSelectionChanged.addListener((tabId, selectInfo) => {
+//     console.log(tabId);
+//     console.log(selectInfo);
+// });
+
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    if (tabId == lastActiveTabData.tabId) {
+        saveSessionData(lastActiveTabData.tabId, lastActiveTabData.windowId, lastActiveTabData.hostName, null);
+    }
+});
